0ece504d15f4b81af14f6782652d3a51b60f3757
Jordan Miller <jordan.kay@gmail.com>
2019-08-24 11:24:50 -0600

    commit

--- docker_run.bat
+++ docker_run.bat
@@ -4,4 +4,4 @@ docker run ^
 -p 5001:5001 ^
 -v %cd%:/app ^
 paradoxlabs/knot ^
-/bin/bash -c "nohup jupyter lab --ip=0.0.0.0 --port=5001 --no-browser --allow-root > /app/logs/jupyter.log 2>&1"
+/bin/bash -c "nohup jupyter lab --ip=0.0.0.0 --port=5001 --no-browser --allow-root > /dev/null 2>&1"
--- docker_run.sh
+++ docker_run.sh
@@ -2,6 +2,6 @@ docker run \
 --rm -it \
 -p 5000:5000 \
 -p 5001:5001 \
--v %cd%:/app \
+-v $(pwd):/app \
 paradoxlabs/knot \
-nohup jupyter lab --ip=0.0.0.0 --port=5001 --no-browser --allow-root > /app/logs/jupyter.log 2>&1
+nohup jupyter lab --ip=0.0.0.0 --port=5001 --no-browser --allow-root > /dev/null 2>&1
